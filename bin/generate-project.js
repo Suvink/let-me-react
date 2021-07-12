7ee6c9f407c6c45911f81ac1e95b0f87cfb45dab
suvink <suvink456@yahoo.com>
2021-07-13 01:13:22 +0530

    Initial Commit

---
+++ bin/generate-project.js
@@ -0,0 +1,131 @@
+#! /usr/bin/env node
+
+'use strict';
+
+const path = require('path');
+const util = require('util');
+const packageJson = require('../package.json');
+const fs = require('fs');
+const exec = util.promisify(require('child_process').exec);
+
+async function runCmd(command) {
+  try {
+    const { stdout, stderr } = await exec(command);
+    console.log(stdout);
+    console.log(stderr);
+  } catch {
+    (error) => {
+      console.log('\x1b[31m', error, '\x1b[0m');
+    };
+  }
+}
+
+if (process.argv.length < 3) {
+  console.log('\x1b[31m', 'You have to provide name to your app.');
+  console.log('For example:');
+  console.log('    npx let-me-react my-app', '\x1b[0m');
+  process.exit(1);
+}
+
+const ownPath = process.cwd();
+const folderName = process.argv[2];
+const appPath = path.join(ownPath, folderName);
+const repo = 'https://github.com/Suvink/let-me-react.git';
+
+try {
+  fs.mkdirSync(appPath);
+} catch (err) {
+  if (err.code === 'EEXIST') {
+    console.log(
+      '\x1b[31m',
+      `The file ${appName} already exist in the current directory, please give it another name.`,
+      '\x1b[0m'
+    );
+  } else {
+    console.log(err);
+  }
+  process.exit(1);
+}
+
+async function setup() {
+  try {
+    console.log('\x1b[33m', 'Downloading the project structure... So පොඩ්ඩක් ඉඳපන්!', '\x1b[0m');
+    await runCmd(`git clone --depth 1 ${repo} ${folderName}`);
+
+    process.chdir(appPath);
+
+    console.log('\x1b[34m', 'Installing dependencies...', '\x1b[0m');
+    await runCmd('npm install');
+    console.log();
+
+    await runCmd('npx rimraf ./.git');
+
+    fs.unlinkSync(path.join(appPath, 'LICENSE.MD'));
+    fs.rmdirSync(path.join(appPath, 'bin'), { recursive: true });
+    fs.unlinkSync(path.join(appPath, 'package.json'));
+
+    buildPackageJson(packageJson, folderName);
+
+    console.log(
+      '\x1b[32m',
+      'The installation is done, this is ready to use !',
+      '\x1b[0m'
+    );
+    console.log();
+
+    console.log('\x1b[34m', 'You can start by typing:');
+    console.log(`    cd ${folderName}`);
+    console.log('    npm start', '\x1b[0m');
+    console.log();
+    console.log('Check Readme.md for more informations');
+    console.log();
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+setup();
+
+function buildPackageJson(packageJson, folderName) {
+  const {
+    bin,
+    keywords,
+    license,
+    homepage,
+    repository,
+    bugs,
+    ...newPackage
+  } = packageJson;
+
+  Object.assign(newPackage, {
+    name: folderName,
+    version: '1.0.0',
+    description: 'A sample ReactJS App Created with Let-Me-React',
+    author: '',
+    scripts: {
+        start: "react-scripts start",
+        build: "react-scripts build",
+        test: "react-scripts test",
+        eject: "react-scripts eject"
+    },
+      dependencies: {
+          "@testing-library/jest-dom": "^5.11.4",
+          "@testing-library/react": "^11.1.0",
+          "@testing-library/user-event": "^12.1.10",
+          "bulma": "^0.9.2",
+          "notistack": "^1.0.9",
+          "react": "^17.0.2",
+          "react-dom": "^17.0.2",
+          "react-router-dom": "^5.2.0",
+          "react-scripts": "4.0.3",
+          "sweetalert2": "^11.0.18",
+          "web-vitals": "^1.0.1"
+      },
+  });
+
+  fs.writeFileSync(
+    `${process.cwd()}/package.json`,
+    JSON.stringify(newPackage, null, 2),
+    'utf8'
+  );
+}
