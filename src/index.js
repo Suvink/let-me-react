5f41648156c7766203515059acd700ba7d1faa30
suvink <suvink456@yahoo.com>
2021-07-14 19:21:54 +0530

    More scripts added

--- src/index.js
+++ src/index.js
@@ -2,11 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { SnackbarProvider } from 'notistack';
 import reportWebVitals from './reportWebVitals';
+import 'bulma/css/bulma.min.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <SnackbarProvider maxSnack={3}>
+      <App />
+    </SnackbarProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
