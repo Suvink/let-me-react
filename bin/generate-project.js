3e9000f007e14ac882b0dd9e360490cbebdfdcaa
suvink <hello@suvin.me>
2022-05-11 09:50:38 +0530

    Remove CHANGELOG upon creation

--- bin/generate-project.js
+++ bin/generate-project.js
@@ -65,6 +65,7 @@ async function setup() {
     fs.rmSync(path.join(appPath, "bin"), { recursive: true });
     fs.unlinkSync(path.join(appPath, "package.json"));
     fs.unlinkSync(path.join(appPath, "package-lock.json"));
+    fs.unlinkSync(path.join(appPath, "CHANGELOG.md"));
 
     console.log("\x1b[36m", "⌛  Creating a new package...", "\x1b[0m");
     await buildPackageJson(packageJson, folderName);
