86da10e750cc20255985015d413833321a17d481
Neranjana Prasad <neranjanaprsd@gmail.com>
2023-10-05 18:44:25 +0530

    Added a navbar template

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import React from "react";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-} from "react-router-dom";
-import HomePage from './screens/HomePage';
-import LoginPage from './screens/LoginPage';
-import AboutUsPage from './screens/AboutUsPage';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import HomePage from "./screens/HomePage";
+import LoginPage from "./screens/LoginPage";
+import AboutUsPage from "./screens/AboutUsPage";
+import NavBar from "./common/NavBar";
 
 function App() {
   return (
     <Router>
+      <NavBar />
+      
       <Switch>
         <Route path="/" exact>
           <HomePage />
