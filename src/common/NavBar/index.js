a76f4af6e4662e78f6b4c5ee4e0a47a630c237dd
Neranjana Prasad <neranjanaprsd@gmail.com>
2023-10-07 12:44:42 +0530

    Fixed issue #14

---
+++ src/common/NavBar/index.js
@@ -0,0 +1,65 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { useState } from "react";
+
+const NavBar = () => {
+  const [isActive, setisActive] = useState(false);
+
+  const routes = [
+    {
+      href: "/",
+      label: "Home",
+    },
+    {
+      href: "/about",
+      label: "About",
+    },
+  ];
+
+  return (
+    <nav className="navbar" role="navigation" aria-label="main navigation">
+      <div className="navbar-brand">
+        <Link to="/" className="navbar-item">
+          <img src="/logo192.png" height="28" />
+        </Link>
+
+        <a
+          role="button"
+          className="navbar-burger"
+          aria-label="menu"
+          aria-expanded="false"
+          data-target="navbarBasicExample"
+          onClick={() => {
+            setisActive(!isActive);
+          }}
+        >
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
+      </div>
+
+      <div id="navbarBasicExample" className={`navbar-menu ${isActive ? "is-active" : ""}`}>
+        <div className="navbar-start">
+          {routes.map((route) => (
+            <Link to={route.href} className="navbar-item">
+              {route.label}
+            </Link>
+          ))}
+        </div>
+
+        <div className="navbar-end">
+          <div className="navbar-item">
+            <div className="buttons">
+              <Link to="/login" className="button is-primary">
+                <strong>Login</strong>
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
