f39dc905f541e34506a18e5f64d9969108f383b6
Suvin Nimnaka <hello@suvin.me>
2025-10-19 12:02:38 +0530

    Update project to support vite

---
+++ templates/app/AppTemplate.tsx
@@ -0,0 +1,25 @@
+import React, { useEffect } from "react";
+import { BrowserRouter, Routes, Route } from "react-router";
+import AOS from "aos";
+import AboutPage from "./screens/AboutPage";
+import HomePage from "./screens/HomePage";
+import LoginPage from "./screens/LoginPage";
+
+const App = (): React.FC => {
+    
+    useEffect(() => {
+        AOS.init();
+    }, []);
+
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/about" element={<AboutPage />} />
+                <Route path="/login" element={<LoginPage />} />
+            </Routes>
+        </BrowserRouter>
+    );
+};
+
+export default App;
---
+++ templates/screens/ScreenTemplate.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import reactLogo from "../../assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./style.css";
+
+const {{ .SCREEN_NAME }} = () => {
+    return (
+        <section className="hero is-fullheight">
+            <div className="hero-body is-flex-direction-column is-justify-content-center">
+                <div className="text-holder has-text-centered">
+                    <p className="title is-1">This is your {{ .SCREEN_NAME }}</p>
+                    
+                </div>
+                <div className="is-flex mb-5 mt-5">
+                    <img src={viteLogo} className="logo vite mr-3" alt="Vite logo" />
+                    <img src={reactLogo} className="logo react ml-3" alt="React logo" />
+                </div>
+                <div className="text-holder has-text-centered">
+                    <p className="title is-3 mb-1">Vite + React</p>
+                    <p>Made with ❤️ using Let Me React</p>
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default {{ .SCREEN_NAME }};
